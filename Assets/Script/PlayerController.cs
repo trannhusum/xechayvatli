9d5fc18bb5a40bccc1435be906c5d7a834ebe51a
trannhusum <transum30052005@gmail.com>
2024-04-20 13:41:06 +0700

    Update

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveForce = 10f; // Lực di chuyển
+    public float rotationForce = 100f; // Lực quay
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Lấy giá trị đầu vào từ Input Manager
+        float moveInput = Input.GetAxis("Vertical");
+        float rotateInput = Input.GetAxis("Horizontal");
+
+        // Áp dụng lực di chuyển
+        Vector3 moveDirection = transform.forward * moveInput * moveForce;
+        rb.AddForce(moveDirection);
+
+        // Áp dụng lực quay
+        Vector3 rotationTorque = Vector3.up * rotateInput * rotationForce;
+        rb.AddTorque(rotationTorque);
+    }
+}
+
